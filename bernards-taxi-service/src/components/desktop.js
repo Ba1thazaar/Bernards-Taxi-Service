4db05f8dc339d8c5055c4eca7091802f1dd50bac
Ba1thazaar <smithwe888@gmail.com>
2022-07-25 12:59:47 -0600

    v1.0

--- bernards-taxi-service/src/components/desktop.js
+++ bernards-taxi-service/src/components/desktop.js
@@ -3,9 +3,48 @@ import './desktop.styles.css'
 
 function Desktop () {
    return (
-     <div>
-       <img className = "logo" src = {require("../images/BTSLogo.jpg")} alt = "Zia logo with BTS inside"/>
+      <div className = "desktop">
+        <div id = "menuHeader">
+          <h3>Booking</h3>
+          <h3>Availability</h3>
+          <h3>Locations</h3>
+        </div>
+       <img className = "logo" src = {require("../images/DesktopBTSBanner.jpg")} alt = "Zia logo with BTS inside"/>
        <h1>Bernards Taxi Service</h1>
+       <a>Providing reliable and certified airport transportation service in Santa Fe and more!</a>
+       <div className = "row">
+        <div className = "column">
+          <h2> Locations</h2>
+          <div className = "center">
+            <ul>
+              <li>Albaquerque Airport </li>
+              <li>Santa Fe Airport</li>
+              <li>Taos Airport and Taos Pueblo</li>
+              <li>Los Alamos</li>
+              <li>Meow Wolf</li>
+              <li>10,000 Waves Spa</li>
+              <li>Ojo Spa and Hotsprings</li>
+              <li>Bandelier National Monument</li>
+              <li>Ghost Ranch</li>
+              <li>Georgia O'Keefe Museum</li>
+              <li>Tent Rocks National Monument</li>
+              <li>Santuario De Chimayo</li>
+            </ul>
+          </div>
+        </div>
+        <div className = "column">
+          <h2>Booking</h2>
+          <a>Please call or text for reservations at: </a>
+          <a><b>Bernard:</b> 505-550-9901</a>
+          <a><b>Preety:</b> 505-310-5344</a>
+          <h2>Availability</h2>
+          <a><b>Monday through Friday:</b> 6am - 11pm</a>
+          <a><b>Saturday:</b>8am - 1am</a>
+          <a><b>Sunday:</b>8am - 10pm</a>
+        </div>
+       </div>
+       <div className = "footer">
+       </div>
      </div>
    )
 }
--- bernards-taxi-service/src/components/mobile.js
+++ bernards-taxi-service/src/components/mobile.js
@@ -3,10 +3,40 @@ import './mobile.styles.css'
 
 function Mobile () {
    return (
-     <div>
-       <img className = "logo" src = {require("../images/BTSLogo.jpg")} alt = "Zia logo with BTS inside"/>
-       <h1>Bernards Taxi Service</h1>
-       <h2>Providing reliable and certified taxi service in the Santa Fe area</h2>
+     <div className = "mobile">
+       <img className = "logo" src = {require("../images/MobileBTSBanner.jpg")} alt = "Zia logo with BTS inside"/>
+       <div className = "Section">
+         <h1>Bernards Taxi Service</h1>
+         <h3>Providing reliable and certified airport transportation service in and around Santa Fe!</h3>
+       </div>
+       <h2>Locations</h2>
+       <div className = "Section">
+         <ul>
+           <li>Albaquerque Airport </li>
+           <li>Santa Fe Airport</li>
+           <li>Taos Airport and Taos Pueblo</li>
+           <li>Los Alamos</li>
+           <li>Meow Wolf</li>
+           <li>10,000 Waves Spa</li>
+           <li>Ojo Spa and Hotsprings</li>
+           <li>Bandelier National Monument</li>
+           <li>Ghost Ranch</li>
+           <li>Georgia O'Keefe Museum</li>
+           <li>Tent Rocks National Monument</li>
+           <li>Santuario De Chimayo</li>
+         </ul>
+       </div>
+       <div className = "Section">
+       <h2>Booking</h2>
+         <a>Please call or text for reservations at: </a>
+         <b>Bernard:</b><a href="tel:505-550-9901"> 505-550-9901</a>
+         <b>Preety:</b> <a href="tel:505-310-5344"> 505-310-5344</a>
+       </div>
+       <div className = "Section">
+         <h2>Availability</h2>
+         <a><b>On Call Hours:</b>  5 am to 11pm</a>
+         <a>Reservations available 24/7</a>
+       </div>
      </div>
    )
 }
