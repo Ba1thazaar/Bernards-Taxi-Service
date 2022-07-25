ab7954804b18cb91ac5f33270cf2c633a2e1268e
Ba1thazaar <smithwe888@gmail.com>
2022-07-25 14:15:47 -0600

    hello react

--- bernards-taxi-service/src/index.js
+++ bernards-taxi-service/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const myFirstElement = <h1>Hello React!</h1>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myFirstElement)
